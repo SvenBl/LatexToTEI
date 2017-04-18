30f8ec51c2e3f2aafe88a71b351c125accbbe2f4
SvenBl <sven-blanck@web.de>
2017-04-18 19:03:33 +0200

    Test with the new Reader

--- src/Main.java
+++ src/Main.java
@@ -1,51 +1,29 @@
+import java.util.List;
+
 /**
  * Created by disas on 18.04.2017.
  */
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 
     public static void main(String[]args){
 
-        //Define File Path
-        List<String> file = readFile("C:\\Users\\disas\\Dropbox\\Uni Leipzig\\Anwendungen der Linguistischen Informatik\\CLARIN_CTS.tex");
+        LatexReader reader = new LatexReader("C:\\Users\\disas\\Dropbox\\Uni Leipzig\\Anwendungen der Linguistischen Informatik\\CLARIN_CTS.tex");
+        List<String> latexHeader = reader.getLatexHeader();
+        List<String> latexText = reader.getLatexText();
 
-        Header header = new Header();
-
-
-        List<String> headerList = header.getHeaderList();
-
-        for (String s : headerList) {
+        for(String s : latexHeader)
+        {
             System.out.println(s);
         }
 
+        System.out.println("------------------------------Trenner----------------------------------------------");
+
+        for(String s : latexText)
+        {
+            System.out.println(s);
+        }
 
     }
 
-
-    //Create List for File
-    private static List<String> readFile(String filename)
-    {
-        List<String> records = new ArrayList<String>();
-        try
-        {
-            BufferedReader reader = new BufferedReader(new FileReader(filename));
-            String line;
-            while ((line = reader.readLine()) != null)
-            {
-                records.add(line);
-            }
-            reader.close();
-            return records;
-        }
-        catch (Exception e)
-        {
-            System.err.format("Exception occurred trying to read '%s'.", filename);
-            e.printStackTrace();
-            return null;
-        }
-    }
 }
